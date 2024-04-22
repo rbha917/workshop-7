389b1ac041fe01e9881091eee81f5cb4d0530aca
Roshni <162877854+rbha917@users.noreply.github.com>
2024-04-23 10:00:58 +1200

    correct file

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
+let walkers = []
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+
+  for(s = 0; s < 40; s ++){
+    let x = random(0, width);
+    let y = random(0, height);
+    let r = random(0, 255);
+    let g = random(0, 255);
+    let b = random(0, 255);
+    walkers[s] = new Walker(x, y, r, g, b);
+  }
+}
+
+class Walker{
+
+	constructor(x, y, r, g, b){
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
+	step(){
+		this.x += random(-2, 2);
+		this.y += random(-2, 2);
+	}
+
+	show(){
+		strokeWeight(2);
+		stroke(this.r, this.g, this.b);
+		point(this.x, this.y);
+	}
 }
 
 function draw() {
-  background(220);
+  background(0);
+  for(x = 0; x < walkers.length; X++){
+    walkers[x].step();
+    walkers[x].show();
+  }  
 }
+
+
+

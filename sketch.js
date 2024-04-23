c66b2873fd909b16362bb51676ef04687b34dc04
Roshni <162877854+rbha917@users.noreply.github.com>
2024-04-23 20:06:50 +1200

    walker + balls

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let walkers = []
+let balls = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +13,13 @@ function setup() {
     let b = random(0, 255);
     walkers[s] = new Walker(x, y, r, g, b);
   }
+
+  for(c = 0; c < 30; c ++){ //number is amount of the object//
+    let rx = random(15, width - 15);
+    let ry = random(15, height - 15);
+    let rr = random(10, 50)
+    balls[c] = new Ball(rx, ry, rr);
+  }  
 }
 
 class Walker{
@@ -36,12 +44,53 @@ class Walker{
 	}
 }
 
+class Ball {
+	constructor(x, y, r){ //where you can define the variables to use in the functions, this one defines the starting x and y positions//
+		that.x = x;
+		that.y = y;
+		that.r = r
+	} //you can just leave these as x and y, no number values and then add in the values under setup between the () when you write 'new Ball' if you want them to be different//
+	
+	move(){
+	ball.x = ball.x + random(-4, 4);
+	ball.y = ball.y + random(-4, 4);		
+	}
+
+	show(){
+		fill(255, 0, 255);
+		noStroke();
+		ellipse(that.x, that.y, 30, 30);
+	}
+
+	edges(){ //prevent object from going off the canvas completely, number determined by half size of object//
+		if (that.x < 15){
+			that.x = 15;
+		} else if(that.x > width - 15){
+			that.x = width - 15;
+		}
+		if (that.y < 15){
+			that.y = 15;
+		} else if(that.y > height - 15){
+			that.y = height - 15;
+		}
+	}
+}
+
 function draw() {
   for(x = 0; x < walkers.length; x++){
     walkers[x].step();
     walkers[x].show();
+  }
+  
+  for(x = 0; x < balls.length; x++){
+    balls[x].move();
+    balls[x].show();
+    balls[x].edges();
   }  
 }
+
+
+
 
 
 
